a7a02a4e6a17b54d3fffb8bee15ef706c9d2cc3b
cldme <ioncld97@gmail.com>
2018-08-06 12:51:27 +0200

    Updated .gitignore file

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => {
-    return (
-        <Modal
-            isOpen={!!props.selectedOption}
-            onRequestClose={props.handleHideModal}
-            contentLabel="Selected Option"
-            ariaHideApp={false}
-        >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleHideModal}>Close</button>
-        </Modal>
-    );
-};
+const OptionModal = (props) => (
+    <Modal
+        isOpen={!!props.selectedOption}
+        onRequestClose={props.handleHideModal}
+        contentLabel="Selected Option"
+        ariaHideApp={false}
+    >
+        <h3>Selected Option</h3>
+        {props.selectedOption && <p>{props.selectedOption}</p>}
+        <button onClick={props.handleHideModal}>Close</button>
+    </Modal>
+);
 
 export default OptionModal;
